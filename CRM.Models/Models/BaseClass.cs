f7fd338821dcf5ecb71454e881a08b56b92a9a39
Mike <mike.ash@lexicon.se>
2017-02-16 15:23:49 +0100

    _Base

--- CRM.Models/Models/BaseClass.cs
+++ CRM.Models/Models/BaseClass.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CRM.Models.Models
 {
     public class BaseClass
     {
+        public long ID { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+        public long AssignedTo { get; set; }
+        public string AssignedToUserGroup { get; set; }
     }
 }
