12d4519aa3795d0dfd7fddf810a8cc829a3b3197
Mike <mike.ash@lexicon.se>
2017-02-17 08:12:11 +0100

    _Web

---
+++ CRM.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRM.Web.Startup))]
+namespace CRM.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
